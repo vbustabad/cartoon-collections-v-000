7b090eac85880ceaf1c3d06c20f7d0df17b25d94
Veronica Bustabad <vbustabad@gmail.com>
2018-02-15 22:11:34 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def summon_captain_planet(planeteer_calls)
   end
 end
 
-def long_planeteer_calls(calls)
+def long_planeteer_calls
   calls.include? do |call|
     if call.length > 4
       return true
