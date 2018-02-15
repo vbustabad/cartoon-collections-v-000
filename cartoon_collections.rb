62e3f60beb1a12124a066b0ba01abbdbb709ca54
Veronica Bustabad <vbustabad@gmail.com>
2018-02-15 21:52:17 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,15 +11,19 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.include? do |call|
-    if call.length > 4
+  if calls.include? do |call|
+    call.length > 4
     return true
-    else
+  else
     return false
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(snacks)
+cheese_types = ["cheddar", "gouda", "camembert"]
+  if snacks.find do |snack|
+    snack == cheese_types[0] || snack == cheese_types[1] || snack == cheese_types[2]
+  else 
+    return nil
+  end
 end
